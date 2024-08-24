a78197f1bf19da29e87095d671ca42cc8689dc47
Jeremy Paule Pereira <jeremy.devpaulepereira@gmail.com>
2024-08-23 22:48:33 -0300

    ReciMe Challenge Project: First Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ddg0cip9uom1w.cloudfront.net",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
